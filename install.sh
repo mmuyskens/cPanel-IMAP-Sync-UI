03d529885573747c1bce8adbaf20ddbcf3e841be
mmuyskens <mmuyskens@gmail.com>
2020-07-23 11:25:39 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync
 cd /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync
 wget https://github.com/mmuyskens/cPanel-IMAP-Sync-UI/raw/master/loop9_imapsync.tar.gz
 tar xzf loop9_imapsync.tar.gz
+cd loop9_imapsync/
+mv * ../
 chown -R root:root /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/
 chmod 755 /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/loop9_imapsync.tar.gz
