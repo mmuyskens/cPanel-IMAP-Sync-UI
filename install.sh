24a4eb163792fbf669b5508d88ec714040701350
mmuyskens <mmuyskens@gmail.com>
2022-09-27 13:49:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # PURPOSE: Install IMAP Sync cPanel UI
 # AUTHOR: Mark Muyskens <me@markmuyskens.com>
 #
-echo "Installing IMAP Sync UI"
+echo "Installing IMAP Sync UI - Paper_Lantern"
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync
 cd /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync
 wget https://github.com/mmuyskens/cPanel-IMAP-Sync-UI/raw/master/loop9_imapsync.tar.gz
@@ -13,4 +13,16 @@ mv * ../
 chown -R root:root /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/
 chmod 755 /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/loop9_imapsync/loop9_imapsync.tar.gz
+echo "Installation is complete!"
+
+echo "Installing IMAP Sync UI - Jupiter"
+mkdir -p /usr/local/cpanel/base/frontend/jupiter/loop9_imapsync
+cd /usr/local/cpanel/base/frontend/jupiter/loop9_imapsync
+wget https://github.com/mmuyskens/cPanel-IMAP-Sync-UI/raw/master/loop9_imapsync.tar.gz
+tar xzf loop9_imapsync.tar.gz
+cd loop9_imapsync/
+mv * ../
+chown -R root:root /usr/local/cpanel/base/frontend/jupiter/loop9_imapsync/
+chmod 755 /usr/local/cpanel/base/frontend/jupiter/loop9_imapsync/
+/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/loop9_imapsync/loop9_imapsync.tar.gz
 echo "Installation is complete!"
